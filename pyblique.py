6f81e883509942b0c7075998488b3d9a514d552c
rburgosnavas <roby0b@gmail.com>
2013-04-17 16:48:15 -0700

    terminal screen clears before printing output.

--- pyblique.py
+++ pyblique.py
@@ -1,18 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.3
 
 '''
 Created on Apr 14, 2013
 
 @author: roberto
 '''
-import sqlite3 as sql, random as rand
+import sqlite3 as sql, random as rand, subprocess as sub, platform
 
+# load strategies from file
 def load():
     oblique = open('oblique', 'r')
     oblique_list = oblique.readlines()
     oblique.close()
     return oblique_list
 
+# drop the table
 def drop():
     conn = sql.connect('oblique.db')
     cursor = conn.cursor()
@@ -20,6 +22,7 @@ def drop():
     conn.commit()
     cursor.close()
 
+# create the table
 def create():
     conn = sql.connect('oblique.db')
     cursor = conn.cursor()
@@ -27,6 +30,7 @@ def create():
     conn.commit()
     cursor.close()
 
+# insert to table
 def insert(oblique_list):
     conn = sql.connect('oblique.db')
     cursor = conn.cursor()
@@ -35,12 +39,14 @@ def insert(oblique_list):
         cursor.execute("insert into strategies(strategy) values (?)", (strategy.strip(), ))    
     conn.commit()
     cursor.close()
-    
+
+# execute sqlite commands    
 def execute():
     create()
     oblique_list = load()
     insert(oblique_list)
-    
+
+# get random strategy from db    
 def get_strategy():
     strategy_id = rand.randint(1,195)
     conn = sql.connect('oblique.db')
@@ -50,16 +56,26 @@ def get_strategy():
     cursor.close()
     return strategy
 
+# clears the terminal screen before printing strategy
+def clear():
+    sub.Popen("cls" if platform.system() == "Windows" else "clear", shell=False)
+
+# print a line of stars that's 6 stars longer than the length of the text
 def print_star_line(text):
     for i in range(len(text) + 6):
         print("*", end='')
 
+# print strategy text with a frame of stars around it just to make it pretty
 def print_strategy(strategy):
+    print("\n")
     print_star_line(strategy)
-    print("\n** " + strategy + " **")
+    print("\n*  " + strategy + "  *")
     print_star_line(strategy)
+    print("\n")
 
+# main
 def main():
+    clear()
     print_strategy(get_strategy())
     
 # execute()
